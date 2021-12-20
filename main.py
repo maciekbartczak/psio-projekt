b926b2cb3a67adccfcc06c7fb010f688fba53d48
maciekbartczak <maciekbartczak17@gmail.com>
2021-12-20 21:52:08 +0100

    primitive player implementation

--- main.py
+++ main.py
@@ -6,17 +6,20 @@ pygame.init()
 width, height = 1000, 800
 
 obstacle_size = 50;
-obstacle_speed = 10;
+obstacle_speed = 5;
+player_speed = 0;
 
 obstacle_x = random.randrange(0, width)
 obstacle_y = -50
+
+player_x = 475
 
 display = pygame.display.set_mode((width, height))
 
 clock = pygame.time.Clock()
 
-def draw_obstacle(display, x, y, size):
-    pygame.draw.rect(display, (0, 0, 0), [x, y, size, size])
+def draw_rect(display, x, y, size, color):
+    pygame.draw.rect(display, color, [x, y, size, size])
 
 while True:
     
@@ -25,11 +28,23 @@ while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a:
+                player_speed = -5
+            if event.key == pygame.K_d:
+                player_speed = 5
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_a or event.key == pygame.K_d:
+                player_speed = 0
 
     display.fill((255, 255, 255))
-    draw_obstacle(display, obstacle_x, obstacle_y, obstacle_size)
+    draw_rect(display, obstacle_x, obstacle_y, obstacle_size, (255, 0, 0))
+    draw_rect(display, player_x, height - obstacle_size, obstacle_size, (0, 0, 255))
 
     obstacle_y += obstacle_speed
+    
+    if player_x + player_speed >= 0 and player_x + player_speed + obstacle_size <= width:
+        player_x += player_speed
 
     if obstacle_y > height:
         obstacle_y = -50
